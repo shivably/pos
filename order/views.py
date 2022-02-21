0fc8d57c07e90ac0f4a55598fdb5471f6bae26e4
Senthil <senthilprakash2020@gmail.com>
2022-02-21 19:28:33 +0530

    first commit

---
+++ order/views.py
@@ -0,0 +1,245 @@
+from django.views.generic import ListView, CreateView, UpdateView
+from django.utils.decorators import method_decorator
+from django.contrib.admin.views.decorators import staff_member_required
+from django.shortcuts import get_object_or_404, redirect, reverse
+from django.urls import reverse_lazy
+from django.contrib import messages
+from django.template.loader import render_to_string
+from django.http import JsonResponse
+from django.db.models import Sum
+from django_tables2 import RequestConfig
+from .models import Order, OrderItem, CURRENCY
+from .forms import OrderCreateForm, OrderEditForm
+from product.models import Product, Category
+from .tables import ProductTable, OrderItemTable, OrderTable
+
+import datetime
+
+
+@method_decorator(staff_member_required(login_url='/login'), name='dispatch')
+class HomepageView(ListView):
+    template_name = 'index.html'
+    model = Order
+    queryset = Order.objects.all()[:10]
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        orders = Order.objects.all()
+        total_sales = orders.aggregate(Sum('final_value'))['final_value__sum'] if orders.exists() else 0
+        paid_value = orders.filter(is_paid=True).aggregate(Sum('final_value'))['final_value__sum']\
+            if orders.filter(is_paid=True).exists() else 0
+        remaining = total_sales - paid_value
+        diviner = total_sales if total_sales > 0 else 1
+        paid_percent, remain_percent = round((paid_value/diviner)*100, 1), round((remaining/diviner)*100, 1)
+        total_sales = f'{total_sales} {CURRENCY}'
+        paid_value = f'{paid_value} {CURRENCY}'
+        remaining = f'{remaining} {CURRENCY}'
+        orders = OrderTable(orders)
+        RequestConfig(self.request).configure(orders)
+        context.update(locals())
+        return context
+
+
+@staff_member_required
+def auto_create_order_view(request):
+    new_order = Order.objects.create(
+        title='Order 66',
+        date=datetime.datetime.now()
+
+    )
+    new_order.title = f'Order - {new_order.id}'
+    new_order.save()
+    return redirect(new_order.get_edit_url())
+
+
+@method_decorator(staff_member_required, name='dispatch')
+class OrderListView(ListView):
+    template_name = 'list.html'
+    model = Order
+    paginate_by = 50
+
+    def get_queryset(self):
+        qs = Order.objects.all()
+        if self.request.GET:
+            qs = Order.filter_data(self.request, qs)
+        return qs
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        orders = OrderTable(self.object_list)
+        RequestConfig(self.request).configure(orders)
+        context.update(locals())
+        return context
+
+
+@method_decorator(staff_member_required, name='dispatch')
+class CreateOrderView(CreateView):
+    template_name = 'form.html'
+    form_class = OrderCreateForm
+    model = Order
+
+    def get_success_url(self):
+        self.new_object.refresh_from_db()
+        return reverse('update_order', kwargs={'pk': self.new_object.id})
+
+    def form_valid(self, form):
+        object = form.save()
+        object.refresh_from_db()
+        self.new_object = object
+        return super().form_valid(form)
+
+
+@method_decorator(staff_member_required, name='dispatch')
+class OrderUpdateView(UpdateView):
+    model = Order
+    template_name = 'order_update.html'
+    form_class = OrderEditForm
+
+    def get_success_url(self):
+        return reverse('update_order', kwargs={'pk': self.object.id})
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        instance = self.object
+        qs_p = Product.objects.filter(active=True)[:12]
+        products = ProductTable(qs_p)
+        order_items = OrderItemTable(instance.order_items.all())
+        RequestConfig(self.request).configure(products)
+        RequestConfig(self.request).configure(order_items)
+        context.update(locals())
+        return context
+
+
+@staff_member_required
+def delete_order(request, pk):
+    instance = get_object_or_404(Order, id=pk)
+    instance.delete()
+    messages.warning(request, 'The order is deleted!')
+    return redirect(reverse('homepage'))
+
+
+@staff_member_required
+def done_order_view(request, pk):
+    instance = get_object_or_404(Order, id=pk)
+    instance.is_paid = True
+    instance.save()
+    return redirect(reverse('homepage'))
+
+
+@staff_member_required
+def ajax_add_product(request, pk, dk):
+    instance = get_object_or_404(Order, id=pk)
+    product = get_object_or_404(Product, id=dk)
+    order_item, created = OrderItem.objects.get_or_create(order=instance, product=product)
+    if created:
+        order_item.qty = 1
+        order_item.price = product.value
+        order_item.discount_price = product.discount_value
+    else:
+        order_item.qty += 1
+    order_item.save()
+    product.qty -= 1
+    product.save()
+    instance.refresh_from_db()
+    order_items = OrderItemTable(instance.order_items.all())
+    RequestConfig(request).configure(order_items)
+    data = dict()
+    data['result'] = render_to_string(template_name='include/order_container.html',
+                                      request=request,
+                                      context={'instance': instance,
+                                               'order_items': order_items
+                                               }
+                                    )
+    return JsonResponse(data)
+
+
+@staff_member_required
+def ajax_modify_order_item(request, pk, action):
+    order_item = get_object_or_404(OrderItem, id=pk)
+    product = order_item.product
+    instance = order_item.order
+    if action == 'remove':
+        order_item.qty -= 1
+        product.qty += 1
+        if order_item.qty < 1: order_item.qty = 1
+    if action == 'add':
+        order_item.qty += 1
+        product.qty -= 1
+    product.save()
+    order_item.save()
+    if action == 'delete':
+        order_item.delete()
+    data = dict()
+    instance.refresh_from_db()
+    order_items = OrderItemTable(instance.order_items.all())
+    RequestConfig(request).configure(order_items)
+    data['result'] = render_to_string(template_name='include/order_container.html',
+                                      request=request,
+                                      context={
+                                          'instance': instance,
+                                          'order_items': order_items
+                                      }
+                                      )
+    return JsonResponse(data)
+
+
+@staff_member_required
+def ajax_search_products(request, pk):
+    instance = get_object_or_404(Order, id=pk)
+    q = request.GET.get('q', None)
+    products = Product.broswer.active().filter(title__startswith=q) if q else Product.broswer.active()
+    products = products[:12]
+    products = ProductTable(products)
+    RequestConfig(request).configure(products)
+    data = dict()
+    data['products'] = render_to_string(template_name='include/product_container.html',
+                                        request=request,
+                                        context={
+                                            'products': products,
+                                            'instance': instance
+                                        })
+    return JsonResponse(data)
+
+
+@staff_member_required
+def order_action_view(request, pk, action):
+    instance = get_object_or_404(Order, id=pk)
+    if action == 'is_paid':
+        instance.is_paid = True
+        instance.save()
+    if action == 'delete':
+        instance.delete()
+    return redirect(reverse('homepage'))
+
+
+@staff_member_required
+def ajax_calculate_results_view(request):
+    orders = Order.filter_data(request, Order.objects.all())
+    total_value, total_paid_value, remaining_value, data = 0, 0, 0, dict()
+    if orders.exists():
+        total_value = orders.aggregate(Sum('final_value'))['final_value__sum']
+        total_paid_value = orders.filter(is_paid=True).aggregate(Sum('final_value'))['final_value__sum'] if\
+            orders.filter(is_paid=True) else 0
+        remaining_value = total_value - total_paid_value
+    total_value, total_paid_value, remaining_value = f'{total_value} {CURRENCY}',\
+                                                     f'{total_paid_value} {CURRENCY}', f'{remaining_value} {CURRENCY}'
+    data['result'] = render_to_string(template_name='include/result_container.html',
+                                      request=request,
+                                      context=locals())
+    return JsonResponse(data)
+
+
+@staff_member_required
+def ajax_calculate_category_view(request):
+    orders = Order.filter_data(request, Order.objects.all())
+    order_items = OrderItem.objects.filter(order__in=orders)
+    category_analysis = order_items.values_list('product__category__title').annotate(qty=Sum('qty'),
+                                                                                      total_incomes=Sum('total_price')
+                                                                                      )
+    data = dict()
+    category, currency = True, CURRENCY
+    data['result'] = render_to_string(template_name='include/result_container.html',
+                                      request=request,
+                                      context=locals()
+                                      )
+    return JsonResponse(data)
---
+++ pos/urls.py
@@ -0,0 +1,33 @@
+from django.contrib import admin
+from django.urls import path
+from django.views.generic.base import TemplateView
+from django.contrib.auth.views import LogoutView
+
+from order.views import (HomepageView, OrderUpdateView, CreateOrderView, delete_order,
+                         OrderListView, done_order_view, auto_create_order_view,
+                         ajax_add_product, ajax_modify_order_item, ajax_search_products, ajax_calculate_results_view,
+                         order_action_view, ajax_calculate_category_view
+                         )
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('login/', TemplateView.as_view(template_name='login.html'), name='home'),
+    path('logout/', LogoutView.as_view(template_name="login.html"), name='logout'),
+    path('', HomepageView.as_view(), name='homepage'),
+    path('order-list/', OrderListView.as_view(), name='order_list'),
+    path('create/', CreateOrderView.as_view(), name='create-order'),
+    path('create-auto/', auto_create_order_view, name='create_auto'),
+    path('update/<int:pk>/', OrderUpdateView.as_view(), name='update_order'),
+    path('done/<int:pk>/', done_order_view, name='done_order'),
+    path('delete/<int:pk>/', delete_order, name='delete_order'),
+    path('action/<int:pk>/<slug:action>/', order_action_view, name='order_action'),
+
+
+    #  ajax_calls
+    path('ajax/search-products/<int:pk>/', ajax_search_products, name='ajax-search'),
+    path('ajax/add-product/<int:pk>/<int:dk>/', ajax_add_product, name='ajax_add'),
+    path('ajax/modify-product/<int:pk>/<slug:action>', ajax_modify_order_item, name='ajax_modify'),
+    path('ajax/calculate-results/', ajax_calculate_results_view, name='ajax_calculate_result'),
+    path('ajax/calculate-category-results/', ajax_calculate_category_view, name='ajax_category_result'),
+
+]
